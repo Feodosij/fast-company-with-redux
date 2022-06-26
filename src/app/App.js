3d105cde287eaa4eb6499f4b712d2b37780fe211
fedya <feodosiyshapovalov@gmail.com>
2022-06-27 02:32:54 +0300

    remove custom hooks,added LogOut+comment

--- src/app/App.js
+++ src/app/App.js
@@ -1,30 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-
 import Users from "./layouts/users";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
 import NavBar from "./components/ui/navBar";
 import { ToastContainer } from "react-toastify";
-import AuthProvider from "./hooks/useAuth";
 import ProtectedRoute from "./components/common/protectedRoute";
 import LogOut from "./layouts/logOut";
-import { useDispatch } from "react-redux";
-import { loadQualitiesList } from "./store/qualities";
-import { loadProfessionsList } from "./store/professions";
-import { loadUsersList } from "./store/users";
+import AppLoader from "./components/ui/hoc/appLoader";
 
 function App() {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(loadQualitiesList());
-        dispatch(loadProfessionsList());
-        dispatch(loadUsersList());
-    }, []);
     return (
         <div>
-            <AuthProvider>
+            <AppLoader>
                 <NavBar />
                         <Switch>
                             <ProtectedRoute
@@ -36,7 +24,7 @@ function App() {
                             <Route path="/" exact component={Main} />
                             <Redirect to="/" />
                         </Switch>
-            </AuthProvider>
+            </AppLoader>
 
             <ToastContainer />
         </div>
